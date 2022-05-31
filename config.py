79497b38fc6cdcc11879d0811198aa269454f317
Anatolii Hulei <togu2503@gmail.com>
2022-05-31 20:02:08 +0300

    Change configs

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = "5520828799:AAHVMscC9OJuKIKS1IxEAsVHxWFkui6E-G8"
-APP_URL = "https://marketing-telegram-bot-228.herokuapp.com/83ceda2f-f193-4142-9559-d8c96f8da532"
+BOT_ENDPOINT = "83ceda2f-f193-4142-9559-d8c96f8da532"
+APP_URL = "https://marketing-telegram-bot-228.herokuapp.com/" + BOT_ENDPOINT
--- main.py
+++ main.py
@@ -7,16 +7,19 @@ from config import *
 bot = telebot.TeleBot(token=BOT_TOKEN)
 server = Flask(__name__)
 
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, 'Welcome with webhook!')
 
-@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+
+@server.route(f"/{}", methods=['POST'])
 def redirect_message():
     json = request.get_data().decode("utf-8")
     update = telebot.types.Update.de_json(json)
     bot.process_new_updates([update])
     return "ok", 200
+
 
 if __name__ == "__main__":
     bot.remove_webhook()
